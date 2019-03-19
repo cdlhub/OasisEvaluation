d57335e2cc10c3338cf8957999e0e2a047ce4966
Camille Daum-Lobko <camille@daum-lobko.com>
2019-03-19 16:12:15 -0400

    Remove unecessary -y

--- provision-ansible-sys.sh
+++ provision-ansible-sys.sh
@@ -4,13 +4,13 @@ sudo apt-get update -y
 
 # Python, pip, and boto
 sudo apt-get install python-pip python-dev build-essential -y
-sudo pip install --upgrade pip -y
-sudo pip install --upgrade setuptools -y
-sudo pip install boto -y
+sudo pip install --upgrade pip
+sudo pip install --upgrade setuptools
+sudo pip install boto
 
 # Ansible
 sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:ansible/ansible
+sudo add-apt-repository ppa:ansible/ansible -y
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt-get update -y
 sudo apt-get install ansible -y
