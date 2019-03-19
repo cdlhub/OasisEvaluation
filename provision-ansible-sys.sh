6b0931d96f981fa3d82888f38c0465fc1e36b682
Camille Daum-Lobko <camille@daum-lobko.com>
2019-03-19 15:42:18 -0400

    Add python pip and boto

--- provision-ansible-sys.sh
+++ provision-ansible-sys.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 sudo apt-get update -y
+
+# Python, pip, and boto
+sudo apt-get install python-pip python-dev build-essential -y
+sudo pip install --upgrade pip -y
+sudo pip install --upgrade setuptools -y
+sudo pip install boto -y
+
+# Ansible
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:ansible/ansible
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
