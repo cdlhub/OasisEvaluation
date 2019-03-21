bcf3ab4f1d22960b295402f40f3b2a773fb5ee35
Camille Daum-Lobko <camille@daum-lobko.com>
2019-03-21 09:46:44 -0400

    Move ansible-sys to centos

--- provision-ansible-sys.sh
+++ provision-ansible-sys.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update -y
+sudo yum update -y
 
 # Python, pip, and boto
-sudo apt-get install python-pip python-dev build-essential -y
+sudo yum install python-pip -y
 sudo pip install --upgrade pip
-sudo pip install --upgrade setuptools
 sudo pip install boto
 
 # Ansible
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:ansible/ansible -y
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt-get update -y
-sudo apt-get install ansible -y
+sudo yum install ansible
