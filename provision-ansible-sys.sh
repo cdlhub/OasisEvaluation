9d418262212b8f8b69c3cd415e4a6090e5f88723
Camille Daum-Lobko <camille@daum-lobko.com>
2019-03-21 17:27:44 -0400

    Fix python install

--- provision-ansible-sys.sh
+++ provision-ansible-sys.sh
@@ -3,9 +3,10 @@
 sudo yum update -y
 
 # Python, pip, and boto
+sudo yum install epel-release -y --nogpgcheck
 sudo yum install python-pip -y
 sudo pip install --upgrade pip
 sudo pip install boto
 
 # Ansible
-sudo yum install ansible
+sudo yum install ansible -y
